262090da0084d3111f5267d096ddafd6df8d905c
Helton Denis <heltondeniss@gmail.com>
2018-06-25 23:06:17 -0300

    config states

--- App.js
+++ App.js
@@ -1,40 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    filmes:[]
+  };
+
+  fetch('https://filmespy.herokuapp.com/api/v1/filmes')
+    .then((r)=>r.json())
+    .then((json)=>{
+      let state = this.state;
+      state.filmes = json.filmes;
+      this.setState(state);
+    });
+}
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList
+          data={this.state.filmes}
+          renderItem={({item})=> <Filme data={item} /> }
+          />
       </View>
     );
+  }
+}
+
+class Filme extends Component {
+
+  render() {
+    return(
+      <Text>...</Text>
+      );
   }
 }
 
@@ -44,15 +48,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
